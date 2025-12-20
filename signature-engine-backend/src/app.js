6e8a42e31693a1fe1d3cc047c7daba71f6547c1a
Saurav Kumar <contact.sauravkrkushwaha@gmail.com>
2025-12-20 12:08:27 +0530

    Initial commit

--- signature-engine-backend/src/app.js
+++ signature-engine-backend/src/app.js
@@ -1,5 +1,3 @@
-// src/app.js
-
 import express from "express";
 import cors from "cors";
 import path from "path";
@@ -9,20 +7,18 @@ import apiRoutes from "./routes/index.js";
 
 const app = express();
 
-// For resolving paths correctly in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Middlewares
 app.use(
   cors({
-    origin: "*", // development ke liye open; prod mein restrict kar sakte ho
+    origin: "*",
   })
 );
 
 app.use(
   express.json({
-    limit: "10mb", // base64 signature ke liye thoda zyada
+    limit: "10mb",
   })
 );
 
@@ -32,16 +28,16 @@ app.use(
   })
 );
 
-// Static serving of signed PDFs
-const uploadsAbsolutePath = path.resolve(__dirname, "..", ENV.UPLOAD_DIR);
-app.use("/uploads", express.static(uploadsAbsolutePath));
+// const uploadsAbsolutePath = path.resolve(__dirname, "..", ENV.UPLOAD_DIR);
+// app.use("/uploads", express.static(uploadsAbsolutePath));
 
-// Health check / root
+app.use("/uploads", express.static(ENV.UPLOAD_DIR));
+
+
 app.get("/", (req, res) => {
   res.send("Signature Injection Engine Backend is running.");
 });
 
-// API Routes
 app.use("/api", apiRoutes);
 
 export default app;
